c326eced17555da3dd1b9a5f5c8171d4f97a0740
Antonio Obradović <antonio.obra8@gmail.com>
2023-02-02 23:40:28 +0100

    added new design for header and nft cards

--- client/components/Navbar.jsx
+++ client/components/Navbar.jsx
@@ -6,11 +6,11 @@ const Navbar = () => {
 
   return (
     <nav>
-      <div className="container mx-auto ">
-        <div className="flex justify-between items-center py-5 ">
-          <div className="flex justify-start items-center space-x-10">
+      <div className="container mx-auto">
+        <div className="flex justify-between items-center py-5  ">
+          <div className="flex justify-start items-center space-x-10 z-10">
             <Link href="/">
-              <h5 className="font-semibold text-xl  leading-10 tracking-wide text-amber-200">
+              <h5 className="font-semibold text-xl  leading-10 tracking-wide text-violet-600">
                 cyberpunk nfts
               </h5>
             </Link>
@@ -19,7 +19,7 @@ const Navbar = () => {
               <p className="text-xl text-white">Create</p>
             </Link>
           </div>
-          <div className="flex justify-end items-center space-x-10">
+          <div className="flex justify-end items-center space-x-10 z-10">
             {address ? (
               <>
                 <Link href="/profile">
@@ -31,7 +31,7 @@ const Navbar = () => {
               </>
             ) : (
               <button
-                className="bg-rose-500 py-3 px-5 rounded-3xl text-white font-semibold"
+                className=" bg-violet-700 shadow-[0_20px_50px_rgba(109,_40,_217,_0.7)] py-3 px-5 rounded-3xl text-white font-semibold"
                 onClick={() => connect()}
               >
                 Connect
--- client/components/NftCard.jsx
+++ client/components/NftCard.jsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 
 const NftCard = ({ nft }) => {
   return (
-    <div className=" w-full md:w-[30%] rounded-3xl overflow-hidden cursor-pointer mb-20 ">
+    <div className=" w-full md:w-[30%] rounded-3xl overflow-hidden cursor-pointer mb-20 shadow-[0_20px_50px_rgba(109,_40,_217,_0.15)] hover:shadow-[0_20px_50px_rgba(109,_40,_217,_0.3)] transition-all duration-200 ">
       <Link href={`/nft/${nft.id}`}>
         <div className="flex flex-col relative group">
           <Image
@@ -15,7 +15,7 @@ const NftCard = ({ nft }) => {
             className="w-full h-[400px] object-cover rounded-3xl group-hover:scale-105 transition-all duration-200 ease-in-out "
           />
 
-          <div className="absolute bottom-0 right-0 left-0 bg-[rgba(189,81,81,0.3)]  p-2 rounded-bl-md text-white text-start px-5">
+          <div className="absolute bottom-0 right-0 left-0 bg-[rgba(131,81,189,0.05)]  p-2 rounded-bl-md text-white text-start px-5">
             <h1 className="text-2xl">{nft.name}</h1>
             <p className="text-xl">{nft.price} ETH</p>
           </div>
